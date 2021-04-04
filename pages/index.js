1c55e120f4748b2ddb8c0a10c87b726be63c1af0
admin <bigbassroller@gmail.com>
2021-04-03 19:02:09 -0700

    reduced hero to just title and slug

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import MoreStories from '../components/more-stories'
 import HeroPost from '../components/hero-post'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
-import { getAllPostsForHome } from '../lib/api'
+import { getAllPostsForHome, getAllAemComponents } from '../lib/api'
 import { CMS_NAME } from '../lib/constants'
 
 export default function Index({ allPosts: { edges }, preview }) {
@@ -22,11 +22,8 @@ export default function Index({ allPosts: { edges }, preview }) {
           {heroPost && (
             <HeroPost
               title={heroPost.title}
-              coverImage={heroPost.featuredImage?.node}
-              date={heroPost.date}
-              author={heroPost.author?.node}
               slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
+
             />
           )}
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
